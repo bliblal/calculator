b14cf7fc3791972c11ccf947069c9b6990ee0a60
Bilal <javaid.billal@gmail.com>
2023-09-02 13:31:06 +0500

    add keyboard support

--- script.js
+++ script.js
@@ -173,6 +173,116 @@ function clearState(){
 }
 
 
+
+//keyboard functions
+
+
+function digitpressK(e){
+    console.log(e);
+    if (calcData.firstNumber){
+        let a=calcData.n1;
+        let b=Number(e);
+        let x=`${a}${b}`;
+        console.log(x);
+        populateDisplay(x);
+        calcData.n1=x;
+
+    }
+
+    if (calcData.secondNumber){
+        let c=calcData.n2;
+        let d=Number(e);
+        let y=`${c}${d}`;
+        console.log(y);
+        populateDisplay(y);
+        calcData.n2=y;
+
+    }
+
+}
+
+
+function dotPressK(e){
+    console.log(e);
+    if (!calcData.dotPressed){
+        if(calcData.firstNumber){
+            let j=calcData.n1;
+            let k= e;
+            let l= `${j}${k}`;
+            console.log(l);
+            populateDisplay(l);
+            calcData.n1=l;
+        }
+
+        else if(calcData.secondNumber){
+            let j=calcData.n2;
+            let k= e;
+            let l= `${j}${k}`;
+            populateDisplay(l);
+            calcData.n2=l;
+        }
+
+
+        calcData.dotPressed=true;
+    }
+
+}
+
+
+function operatorPressK(e){
+    if (calcData.firstNumber){
+        calcData.operator=e;
+        calcData.firstNumber=false;
+        calcData.secondNumber=true;
+    }
+
+    else if (calcData.n2=='' && calcData.operator != ''){
+        calcData.operator=e;
+    }
+
+    if (calcData.result != '' && calcData.n1=='' && calcData.n2=='' ){
+        calcData.n1=calcData.result;
+        calcData.result='';
+        calcData.firstNumber=false;
+        calcData.secondNumber=true;
+        calcData.operator=e;
+    }
+
+    if (calcData.n1 != '' && calcData.n2 != ''){
+        let i=(operate(Number(calcData.n1),Number(calcData.n2),calcData.operator));
+        populateDisplay(i);
+        calcData.n1=i;
+        calcData.n2='';
+        calcData.operator=e;
+        calcData.firstNumber=false;
+        calcData.secondNumber=true;
+    }
+
+    calcData.dotPressed=false;
+}
+
+
+function calculateResultK(e){
+    if (calcData.firstNumber){
+        let result=calcData.n1;
+        populateDisplay(result);
+        calcData.result=result;
+    }
+
+    if (calcData.secondNumber){
+        let result;
+        result=operate(Number(calcData.n1),Number(calcData.n2),calcData.operator);
+
+        populateDisplay(result);
+        calcData.result=result;
+    }
+
+    calcData.firstNumber=true;
+    calcData.secondNumber=false;
+    calcData.n1='';
+    calcData.n2='';
+}
+
 //Buttons selector
 let digits=document.querySelectorAll(".num");
 
@@ -202,3 +312,46 @@ period=document.querySelector(".dot");
 
 //period event listener
 period.addEventListener("click",dotPress);
+
+
+//keyboard support
+
+document.addEventListener("keydown", keyPress);
+
+function keyPress(e){
+    console.log(e.key);
+    let k=e.key;
+    if (/^\d+$/.test(k)){
+        console.log(k);
+        digitpressK(k);
+    }
+
+    if (k==='.'){
+        console.log(k);
+        dotPressK(k);
+    }
+
+    if (k==="+" || k==='-'){
+        console.log(k);
+        operatorPressK(k);
+
+    }
+
+    if (k==='*'){
+        operatorPressK("x");
+    }
+
+    if(k==='/'){
+        operatorPressK('÷');
+    }
+
+    if(k==="Enter"){
+        calculateResultK(k);
+    }
+
+
+}
+
+
+
+
